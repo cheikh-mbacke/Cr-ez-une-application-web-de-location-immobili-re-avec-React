408fbec60f2115e40fd0c69e017c6bab7b3c6973
Cheikh Mbacke <mbackecheikhouna7@gmail.com>
2022-10-05 11:15:27 +0200

    cleaning and favicon update

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello React
-        </p>
-        <h1 className="salut">Salut</h1>
-      </header>
+      
     </div>
   );
 }
