3e3920933d6f54b4e0fea0ec6dd6f46c8ad7a3e2
Cheikh Mbacke <mbackecheikhouna7@gmail.com>
2022-10-10 17:28:58 +0200

    errorPage OK

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import About from './pages/About/About';
 import Header from './components/Header/Header';
 import FicheLogement from './pages/FicheLogement/FicheLogement';
 import Footer from "./components/Footer/Footer";
+import ErrorPage from './pages/ErrorPage/ErrorPage';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
 			<main>
 				<Header />
 				<Routes>
+					<Route path="*" element={<ErrorPage />} />
 					<Route path="/" element={<Home />} />
 					<Route path="/about" element={<About />} />
 					<Route path="/logement/id" element={<FicheLogement />} />
