44a5840290a9d16737a185a962f79925bc2b985a
Cheikh Mbacke <mbackecheikhouna7@gmail.com>
2022-10-11 15:43:25 +0200

    début adaptations responsive

--- src/components/Collapse/Collapse.jsx
+++ src/components/Collapse/Collapse.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import './Collapse.scss'
-import Chevron from '../../assets/images/vectorBas.svg'
+import Chevron from '../../assets/images/down-chevron.png'
 
 export default function Collapse() {
   return (
     <div className='collapse-about'>
 
-    <div className='accord-about__visible'>
+    <div className='collapse-about__visible'>
       <h2>Lorem ipsum dolor sit amet.</h2>
       <img src={Chevron} alt="chevron down" />
+    </div>
+
+    <div className="collapse-about__toggle">
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum animi odio quas labore earum culpa assumenda minus. Delectus, deserunt voluptates expedita labore sapiente sequi ex distinctio quasi quia cumque ratione!</p>
     </div>
 
     </div>
