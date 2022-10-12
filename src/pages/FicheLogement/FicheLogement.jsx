4d5b5d64ccd607cdc64ae4b88fab120eedc1e6b5
Cheikh Mbacke <mbackecheikhouna7@gmail.com>
2022-10-12 22:22:53 +0200

    debut Carrousel

--- src/pages/FicheLogement/FicheLogement.jsx
+++ src/pages/FicheLogement/FicheLogement.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import Carrousel from '../../components/Carrousel/Carrousel'
 
 
 export default function FicheLogement() {
 
   return (
     <div>
-      <h1>voici la fiche Logement de l'id :</h1>
+      < Carrousel />
     </div>
   )
 }
