582abc16853e1cd231247b8cb12a36050749468b
Cheikh Mbacke <mbackecheikhouna7@gmail.com>
2022-10-15 15:59:10 +0200

    icon rotation test

--- src/components/Collapse/Collapse.jsx
+++ src/components/Collapse/Collapse.jsx
@@ -1,34 +1,39 @@
-import React, {useState, useRef, useEffect} from 'react'  //import des hooks de base react
-import './Collapse.scss'
-import Chevron from '../../assets/images/vectorBas.svg'
+import React, { useState, useRef, useEffect } from "react"; //import des hooks de base react
+import "./Collapse.scss";
+import Chevron from "../../assets/images/vectorBas.svg";
 
 export default function Collapse(props) {
+	const [toggle, setToggle] = useState(false); // je definie le state du toggle (et false par défaut)
+	const [heightEl, setHeightEl] = useState(); // je definie le state de la hauteur du collapse
 
-  const [toggle, setToggle] = useState(false)  // je definie le state du toggle (et false par défaut)
-  const [heightEl, setHeightEl] = useState()   // je definie le state de la hauteur du collapse
+	const toggleState = () => {
+		//je définie la fonction toggleState qui modifie la valeur toggle au clic
+		setToggle(!toggle);
+	};
 
-  const toggleState = () => {  //je définie la fonction toggleState qui modifie la valeur toggle au clic
-    setToggle(!toggle)
-  }
+	const refHeight = useRef(); //récupère et conserve la valeur de hauteur du collapse déplié
 
-  const refHeight = useRef()  //récupère et conserve la valeur de hauteur du collapse déplié
+	useEffect(() => {
+		setHeightEl(`${refHeight.current.scrollHeight}px`); //useEffect s'éxécute au montage du composant, dans ce cas, il définit la hauteur du collapse déplié lors de sa première ouverture et la conserve dans refHeight
+	}, []);
 
-  useEffect(() => {
-    setHeightEl(`${refHeight.current.scrollHeight}px`) //useEffect s'éxécute au montage du composant, dans ce cas, il définit la hauteur du collapse déplié lors de sa première ouverture et la conserve dans refHeight
-  }, [])
-
-  return ( // affiche le collapse replié par défaut et l'ouvre au clic puis le referme au clic en faisant disparaitre le texte et le style
-		<div className="collapse-about">
+	return (
+		// affiche le collapse replié par défaut et l'ouvre au clic puis le referme au clic en faisant disparaitre le texte et le style
+		<div className="collapse">
 			<div className="collapseContainer">
-				<div onClick={toggleState} className="collapse-about__visible"> 
+				<div onClick={toggleState} className="collapse__visible">
 					<h2>{props.aboutTitle}</h2>
-					<img className='aboutChevron' src={Chevron} alt="chevron down" />
-				</div>  
+					<img
+						className={toggle ? "chevron rotated" : "chevron"}
+						src={Chevron}
+						alt="chevron"
+					/>
+				</div>
 			</div>
 			<div
 				ref={refHeight}
 				className={
-					toggle ? "collapse-about__toggle animated" : "collapse-about__toggle"
+					toggle ? "collapse__toggle animated" : "collapse__toggle"
 				}
 				style={{ height: toggle ? `${heightEl}` : "0px" }}
 			>
