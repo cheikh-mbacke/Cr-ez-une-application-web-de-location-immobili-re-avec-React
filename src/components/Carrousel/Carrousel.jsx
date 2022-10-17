1e32a9bb0a8c8577323d035009ccde28fc90aa59
Cheikh Mbacke <mbackecheikhouna7@gmail.com>
2022-10-17 12:32:58 +0200

    import svg and map pics

--- src/components/Carrousel/Carrousel.jsx
+++ src/components/Carrousel/Carrousel.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './Carrousel.scss'
 
-export default function Carrousel() {
+export default function Carrousel(props) {
   return (
-    <div>
+    <div className='carrousel-container'>
       <h1>Carrousel</h1>
     </div>
   )
--- src/pages/FicheLogement/FicheLogement.jsx
+++ src/pages/FicheLogement/FicheLogement.jsx
@@ -8,30 +8,41 @@ import Tag from "../../components/Tag/Tag";
 import data from "../../datas/logements.json";
 
 export default function FicheLogement() {
+	const ImageData = []
 	const params = useParams();
 	const pickedAppart = data.find(({ id }) => id === params.id);
+	const slidePics = pickedAppart.pictures;
+	slidePics.map((pic) => ImageData.push(pic));
 	const tags = pickedAppart.tags;
 	const equipments = pickedAppart.equipments;
-	const equip = equipments.map((item, index) => <li key={index} className="equipList">{item}</li>);
+	const equip = equipments.map((item, index) => (
+		<li key={index} className="equipList">
+			{item}
+		</li>
+	));
 	return (
 		<div key={params.id} className="fiche-container">
-			<Carrousel />
-			<div className="title-host-container">
-				<div className="ficheTitleContainer">
+			<Carrousel slides={ImageData} />
+			<div className="title-tags-container">
+				<div className="title-container">
 					<h1>{pickedAppart.title}</h1>
 					<h3>{pickedAppart.location}</h3>
 				</div>
-				<Host />
+				<div className="tags-container">
+					{tags.map((tag) => (
+						<Tag key={tag} tag={tag} />
+					))}
+				</div>
 			</div>
-			<div className="tags-container">
-				{tags.map((tag) => (
-					<Tag key={tag} tag={tag} />
-				))}
+			<div className="rate-host-container">
+				<div className="host-container">
+					<Host />
+				</div>
+				<div className="rate-container">
+					<Rate />
+				</div>
 			</div>
-			<div className="rate-container">
-				<Rate />
-			</div>
-			<div className="collapse-fiche-content">
+			<div className="collapse-fiche-container">
 				<Collapse
 					aboutTitle="Description"
 					aboutText={pickedAppart.description}
