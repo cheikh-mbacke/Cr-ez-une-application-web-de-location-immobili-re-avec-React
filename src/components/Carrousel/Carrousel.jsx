c25be3399114553e20733e63a16816d1996fbfe8
Cheikh Mbacke <mbackecheikhouna7@gmail.com>
2022-10-18 13:58:18 +0200

    fix carrousel OK

--- src/components/Carrousel/Carrousel.jsx
+++ src/components/Carrousel/Carrousel.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-import LeftArrow from "../../components/SlideArrows/LeftArrow";
-import RightArrow from "../../components/SlideArrows/RightArrow";
+import left from "../../assets/images/vector-left.svg";
+import right from "../../assets/images/vector-right.svg";
 import "./Carrousel.scss";
 
 export default function Carrousel({ slides }) {
@@ -16,18 +16,22 @@ export default function Carrousel({ slides }) {
 
 	return (
 		<section id="carrousel-container">
-			{length > 1 && <LeftArrow onClick={prevSlide} />}
-			{length > 1 && <RightArrow onClick={nextSlide} />}
+			{length > 1 && <img src={left} alt="gauche" onClick={prevSlide} className="leftArrow" />}
+			{length > 1 && <img src={right} alt="droite" onClick={nextSlide} className="rightArrow" />}
 			{slides.map((slide, index) => (
 				<div key={index}>
 					{index === current && (
 						<img src={slide} alt="appartement à louer" className="slideImage" />
 					)}
 					{index === current && (
-						<span className="slide-number">{current + 1}/{length}</span>
+						<span className="slide-number">
+							{current + 1}/{length}
+						</span>
 					)}
 				</div>
 			))}
 		</section>
 	);
 }
+
+
