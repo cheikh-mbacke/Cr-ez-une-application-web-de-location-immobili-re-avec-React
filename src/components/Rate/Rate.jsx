4791a8de51b88f3b3e9f6a40910f786080d71414
Cheikh Mbacke <mbackecheikhouna7@gmail.com>
2022-10-21 11:24:00 +0200

    rating component OK

--- src/components/Rate/Rate.jsx
+++ src/components/Rate/Rate.jsx
@@ -1,47 +1,29 @@
 import React from "react";
-import star from "../../assets/images/star-rate.svg";
+import emptyStar from "../../assets/images/emptyStar.svg";
+import fullStar from "../../assets/images/star-rate.svg"
 import "../../style/main.scss";
 
-export default function Rate(props) {
-	const stars = props.score;
-	console.log(stars);
+export default function Rate({ score }) {
+	const stars = [1, 2, 3, 4, 5];
 	return (
 		<div className="rate-comp">
-			<span>
-				<img
-					className={stars < 1 ? "star empty" : "star filled"}
-					src={star}
-					alt="rating star"
-				/>
-			</span>
-			<span>
-				<img
-					className={stars < 2 ? "star empty" : "star filled"}
-					src={star}
-					alt="rating star"
-				/>
-			</span>
-			<span>
-				<img
-					className={stars < 3 ? "star empty" : "star filled"}
-					src={star}
-					alt="rating star"
-				/>
-			</span>
-			<span>
-				<img
-					className={stars < 4 ? "star empty" : "star filled"}
-					src={star}
-					alt="rating star"
-				/>
-			</span>
-			<span>
-				<img
-					className={stars < 5 ? "star empty" : "star filled"}
-					src={star}
-					alt="rating star"
-				/>
-			</span>
+			{stars.map((level) =>
+				score >= level ? (
+					<img
+						key={level.toString()}
+						className="star"
+						src={fullStar}
+						alt="rating star"
+					/>
+				) : (
+					<img
+						key={level.toString()}
+						className="star"
+						src={emptyStar}
+						alt="rating star"
+					/>
+				)
+			)}
 		</div>
 	);
 }
