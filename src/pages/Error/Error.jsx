831b426b2a0fe3c7245be267e1558b2e6910e907
Cheikh Mbacke <mbackecheikhouna7@gmail.com>
2022-10-25 20:43:37 +0200

    ok redirect 404 à dig

---
+++ src/pages/Error/Error.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ErrorPage from '../../components/ErrorPage/ErrorPage'
+
+export default function Error() {
+  return (
+    <div>
+      <ErrorPage />
+    </div>
+  )
+}
--- src/pages/FicheLogement/FicheLogement.jsx
+++ src/pages/FicheLogement/FicheLogement.jsx
@@ -10,16 +10,16 @@ import data from "../../datas/logements.json";
 export default function FicheLogement() {
 	const params = useParams();
 	const pickedAppart = data.find(({ id }) => id === params.id);
-	const slidePics = pickedAppart.pictures;
-	const tags = pickedAppart.tags;
-	const equipments = pickedAppart.equipments;
-	const equip = equipments.map((item, index) => (
-		<li key={index} className="equipList">
-			{item}
-		</li>
-	));
-	console.log(pickedAppart);
+	
 	if (pickedAppart !== undefined) {
+		const slidePics = pickedAppart.pictures;
+		const tags = pickedAppart.tags;
+		const equipments = pickedAppart.equipments;
+		const equip = equipments.map((item, index) => (
+			<li key={index} className="equipList">
+				{item}
+			</li>
+		));
 		return (
 			<div key={params.id} className="fiche-container">
 				<Carrousel slides={slidePics} />
