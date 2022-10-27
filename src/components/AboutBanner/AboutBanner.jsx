512e3fe71571d1404be6e3d1343b47526ab0620a
Cheikh Mbacke <mbackecheikhouna7@gmail.com>
2022-10-27 14:01:47 +0200

    About responsive OK

--- src/components/AboutBanner/AboutBanner.jsx
+++ src/components/AboutBanner/AboutBanner.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import '../../style/main.scss'
+import React from "react";
+import "../../style/main.scss";
 
 export default function AboutBanner() {
-  return <div className="aboutBackground"></div>
+	return <div className="aboutBackground wh-msk"></div>;
 }
